d2b4a86232e17d4ae88fbe90e4b1293c0264af27
Tiphanie Jan <jan.tiphanie@outlook.fr>
2024-11-03 20:33:38 +0100

    first commit + ajout de tailwind et du bgHome

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import './index.css';
+import './app.scss';
+
+
+function App () {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App; 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import HomeBackground from "@/components/ui/HomeBackground/HomeBackground";
+import React from "react";
+
+function Home () {
+    return (
+        <div>
+            <HomeBackground/>
+        </div>
+    )
+}
+
+export default Home;
