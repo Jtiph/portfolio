3273f29cbd1c57095339d74c73482d2bda27c49a
Jtiph <161319588+Jtiph@users.noreply.github.com>
2026-05-29 17:43:42 +0200

    Update languageSwitcher.jsx

--- src/components/languageSwitcher/languageSwitcher.jsx
+++ src/components/languageSwitcher/languageSwitcher.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import "./LanguageSwitcher.scss";
+import "./languageSwitcher.scss";
 
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation();
